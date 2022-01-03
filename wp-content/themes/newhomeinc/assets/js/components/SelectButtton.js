ffe5c0649027e8862066cb163f443cf028260c7a
AJ Jardiah <ajardiahjr@gmail.com>
2022-01-03 09:13:24 -0500

    updated baseurl

--- wp-content/themes/newhomeinc/assets/js/components/SelectButtton.js
+++ wp-content/themes/newhomeinc/assets/js/components/SelectButtton.js
@@ -7,7 +7,7 @@ const $window = window.$window || $( window );
 
 const SelectButton = {
 	init() {
-		const getUrl = 'http://newhomeinc.test';
+		const getUrl = 'https://newhomeinc1dev.wpengine.com';
 		( function( $ ) {
 			$( '.detail-page-cta-options' ).on( 'click', function() {
 				$( '.detail-page-cta-slide-out' ).toggleClass( 'slidemodal-hidden' );
