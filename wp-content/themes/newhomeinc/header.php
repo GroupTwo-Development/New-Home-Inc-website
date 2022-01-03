564cf12a22f807e1f5cee82df4ff4bbc615cbf3f
AJ Jardiah <ajardiahjr@gmail.com>
2022-01-03 09:28:42 -0500

    fixed https request

--- wp-content/themes/newhomeinc/header.php
+++ wp-content/themes/newhomeinc/header.php
@@ -15,7 +15,8 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
 </head>
